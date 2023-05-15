7542b0276d0473278e0ec9151c13305e1dbf71bb
rahul074 <rahulraju2628@gmail.com>
2023-05-15 22:46:58 +0100

    Added a layout page and contact us page for EduMate website

--- app/app.js
+++ app/app.js
@@ -31,6 +31,81 @@ app.use(session({
     cookie: { secure: false }
 }));
 
+/* Rahul Raju */
+
+app.get("/contact", function(req, res) {
+    res.render("contact");
+});
+
+app.get("/contactlogin", requireLogin, function(req, res) {
+    if (!req.session.user) {
+        return res.redirect('/login-page');
+    }
+    res.render("contact", { user: req.session.user });
+});
+
+app.post('/send-email', (req, res) => {
+    // Get the form data
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+
+    // Create a transporter for sending email
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: email, // replace with your email
+            pass: 'edumate@admin' // replace with your password
+        }
+    });
+
+    // Set up email options
+    const mailOptions = {
+        from: email, // replace with your email
+        to: "dinsojoseph@gmail.com", // replace with recipient email
+        subject: 'message',
+        html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.redirect('/contact?success=false');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.redirect('/contact?success=true');
+        }
+    });
+});
+
+/* Rahul Raju ENDS */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Start server on port 3000
 app.listen(3000, function() {
     console.log(`Server running at http://127.0.0.1:3000/`);
